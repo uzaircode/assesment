7324e875894a67fa4d8529bba1fa573f81f16e8a
uzaircs <nikuzairsc@gmail.com>
2022-04-22 15:06:23 +0800

    fix button and front end

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:assesment/main.dart';
+import 'package:assesment/section1.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
