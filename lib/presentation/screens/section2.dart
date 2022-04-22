db27092270d86093dfc9f0ea22035a69f6386cf2
uzaircs <nikuzairsc@gmail.com>
2022-04-22 19:09:28 +0800

    new

---
+++ lib/presentation/screens/section2.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:assesment/presentation/screens/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(home: Section2(),));}
+
+class Section2 extends StatelessWidget {
+  const Section2({Key? key}) : super(key: key);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
